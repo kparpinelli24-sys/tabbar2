6d5e1e068507d1092109c51a8711466b40c3d978
kparpinelli24-sys <k.parpinelli.24@gmail.com>
2026-05-26 07:28:49 -0300

    fazendo pastas js

---
+++ src/navigation/Stacknavigator.js
@@ -0,0 +1,16 @@
+import { create, createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from '../screens/HomeScreen';
+import DetailsScreen from './screens/DetailsScreen';
+import ProfileScreen from './screens/ProfileScreen';
+
+const Stack = createStackNavigator();
+const StackNavigator  = () => {
+return (
+<Stack.Navigator initialRouteName="Home">
+<Stack.Screen name="Home" component={HomeScreen} />
+<Stack.Screen name="Details" component={DetailsScreen} />
+<Stack.Screen name="Profile" component={ProfileScreen} />
+</Stack.Navigator>
+);
+};
+export default StackNavigator;
